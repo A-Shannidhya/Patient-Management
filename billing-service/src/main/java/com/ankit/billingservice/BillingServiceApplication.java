4500cb06a041cf0118422e0898e7c0a61e15d6c2
A-Shannidhya <asp45624@gmail.com>
2025-06-07 11:35:00 +0530

    *

---
+++ billing-service/src/main/java/com/ankit/billingservice/BillingServiceApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Ayshi Shannidhya Panda. All rights reserved.
+ *
+ * This source code is confidential and intended solely for internal use.
+ * Unauthorized copying, modification, distribution, or disclosure of this
+ * file, via any medium, is strictly prohibited.
+ *
+ * Project: Patient Management
+ * Author: Ayshi Shannidhya Panda
+ * Created on: 2025-6-7
+ */
+
+package com.ankit.billingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServiceApplication.class, args);
+    }
+
+}
---
+++ billing-service/src/main/java/com/ankit/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025 Ayshi Shannidhya Panda. All rights reserved.
+ *
+ * This source code is confidential and intended solely for internal use.
+ * Unauthorized copying, modification, distribution, or disclosure of this
+ * file, via any medium, is strictly prohibited.
+ *
+ * Project: Patient Management
+ * Author: Ayshi Shannidhya Panda
+ * Created on: 2025-6-7
+ */
+
+package com.ankit.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received {} ", billingRequest.toString());
+
+
+        //Business logic
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+}
